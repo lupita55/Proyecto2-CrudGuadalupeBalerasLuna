c986050e7766b1d5d538dfca624acf787faa315b
lupita55 <gubalderas.c02@ucamp.utel.edu.mx>
2021-09-20 23:33:48 -0500

    Se desgloza el producto pero aún no funciona el editar y eliminar

--- main.js
+++ main.js
@@ -7,16 +7,12 @@ addAgregar.addEventListener("click",function(){
     const addProducto=document.getElementById('producto');
     const addCantidad=document.getElementById('cantidad');
     const addDetalles=document.getElementById('detalles');
-    /*
-    console.log(addProducto.value);
-    console.log(addCantidad.value);
-    console.log(addDetalles.value);
-    */
+    
     var Jason = {'producto':addProducto.value,'cantidad':addCantidad.value,'detalles':addDetalles.value};
     guardarProducto(Jason);
     
     const addLista = document.createElement('li')
-    addLista.innerHTML = "Producto: "+addProducto.value+" Cantidad: "+addCantidad.value+" Detalles: "+addDetalles.value+"<button id='editar' class='editar' type='button' onclick='funEditar();'>Editar</button><button id='eliminar' class='eliminar' type='button' onclick='funEliminar();'>Eliminar</button>";
+    addLista.innerHTML = "<div id="+addProducto.value+"> Producto: "+addProducto.value+" Cantidad: "+addCantidad.value+" Detalles: "+addDetalles.value+"<button id='editar' class='editar' type='button' onclick='funEditar("+addProducto.value+");'>Editar</button><button id='eliminar' class='eliminar' type='button' onclick='funEliminar("+addProducto.value+"\');'>Eliminar</button></div>";
     ListaProd.appendChild(addLista)
 
     addProducto.value="";
@@ -29,5 +25,13 @@ addAgregar.addEventListener("click",function(){
 function guardarProducto(parametro){
     window.localStorage.setItem("producto",JSON.stringify(parametro));
  
+}
 
+function funEditar(){
+    console.log("Entró función editar");
+}
+
+function funEliminar(producto){
+    console.log(producto);
+    document.getElementById(producto).innerHTML="";
 }
